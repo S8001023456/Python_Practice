ae614a5d5a4d23df47cba72207b8132e1151a335
108820029 <jungle4642360@gmail.com>
2020-07-13 16:12:10 +0800

    work for py

--- work_for_python/a130.py
+++ work_for_python/a130.py
@@ -1,16 +1,20 @@
-cases = int(input())
-web_popularity = []
-numeral_cases = []
-for j in range(cases):
-    for i in range(3):
-        reg = list(map(str, input().split()))
-        reg[1] = int(reg[1])
-        web_popularity.append(reg)
-    sources = sorted(web_popularity, key = lambda cmp:cmp[1])
-    numeral_cases.append(sources)
-for numeral_case in range(len(numeral_cases)):
-    print("Case #", numeral_case + 1,sep = "")
-    for source in range(len(sources)):
-        max = numeral_cases[numeral_case][len(sources)-1][1]
-        if numeral_cases[numeral_case][source][1] == max:
-            print(numeral_cases[numeral_case][source][0])
+while True:
+    try:
+        cases = int(input())
+        numeral_cases = []
+        for j in range(cases):
+            web_popularity = []
+            for i in range(10):
+                reg = list(map(str, input().split()))
+                reg[1] = int(reg[1])
+                web_popularity.append(reg)
+            sources = sorted(web_popularity, key = lambda cmp:cmp[1])
+            numeral_cases.append(sources)
+        for numeral_case in range(len(numeral_cases)):
+            print("Case #", numeral_case + 1, ":",sep = "")
+            max = numeral_cases[numeral_case][len(sources)-1][1]
+            for source in range(len(sources)):
+                if numeral_cases[numeral_case][source][1] == max:
+                    print(numeral_cases[numeral_case][source][0])
+    except:
+        break
