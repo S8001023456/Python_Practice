cc5124aa7a4e88eecfda019192f74c74a8e70e1b
108820029 <jungle4642360@gmail.com>
2020-07-15 10:08:42 +0800

    TLE

--- work_for_python/a248.py
+++ work_for_python/a248.py
@@ -4,7 +4,17 @@ while True:
         rem_list = []
         remainder = dividend % divisor
         integer = dividend // divisor
+        count_rem_dig = remainder_digits % 4
+        remainder_digits //= 4
         for i in range(remainder_digits):
+            quotient = remainder * 10000 // divisor
+            if len(str(quotient)) < 5:
+                for plus_zero in range(5 - len(str(quotient))):
+                    rem_list.append(quotient)
+            else:
+                rem_list.append(quotient)
+            remainder = remainder * 10000 % divisor
+        for k in range(count_rem_dig):
             quotient = remainder * 10 // divisor
             rem_list.append(quotient)
             remainder = remainder * 10 % divisor
